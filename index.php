df9a3ab8bab4d8c669174dd255164beaf9bd171c
Peter I & Tony T <peter.iskandar@xtremelabs.com>
2013-06-27 19:55:56 -0400

    EmirH: Fisrt commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+$api_client_id = "1234.apps.googleusercontent.com";
+$api_client_secret = "ITS_A_SECRET_TO_EVERYBODY";
+$api_simple_key = "AIzaSyCCbHcqDeb0oycQ9niV8P3n0F0qM";
+
+$base_url = "emirweb.com";
+
+$sqlite_database = "~/tmp/database.sqlite";
+
+
+
+?>
